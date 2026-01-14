48c38017681b359b9fe88cac41cb4ce50e3e7d41
eksdud001-byte <eksdud001@gmail.com>
2026-01-14 13:24:53 +0900

    aaa

---
+++ front/Javascript/script.js
@@ -0,0 +1,7 @@
+let heading = document.querySelector('#heading');
+    // 오늘 쪽에서 전해주는 벨유의 의미가 중요하다.
+    heading.onclick = function(){
+        // onclick 이벤트다. 클릭하는 이벤트이고 이것말고도 많은 이벤트가 많습니다
+        // 프로그램은 탐정이 범인을 찾는 과정과 같은 것이다.
+        
+    heading.style.color = "purple";}
